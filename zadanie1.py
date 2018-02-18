3691f33fa2af230a28bf7b9407a751e9ffc5e6f7
Grzegorz Juszkiewicz <grzegorzjuszkiewicz@Grzegorzs-MacBook-Air.local>
2018-02-18 23:00:07 +0100

    Skoczone zadanie 1

--- zadanie1.py
+++ zadanie1.py
@@ -1,15 +1,23 @@
 import math
 import numpy as np
-print("Program rozwiazujacy ukaad rawnaa w postaci:\nAx + By = C\nDx + Ey = F")
-A=input("Podaj A= ")
-B=input("Podaj B= ")
-C=input("Podaj C= ")
-D=input("Podaj D= ")
-E=input("Podaj E= ")
-F=input("Podaj F= ")
+print("Program rozwiazujacy uklad rownan w postaci:\nAx + By = C\nDx + Ey = F")
+A=float(input("Podaj A= "))
+B=float(input("Podaj B= "))
+C=float(input("Podaj C= "))
+D=float(input("Podaj D= "))
+E=float(input("Podaj E= "))
+F=float(input("Podaj F= "))
 
-a = np.array([[1, 1],[2,4]])
-b = np.array([35, 94])
-
-print(np.linalg.solve(a,b))
-
+a = np.array([[A, B],[D,E]])
+a_uzup=np.array([[A,B,C],[D,E,F]])
+print(np.linalg.matrix_rank(a))
+print(np.linalg.matrix_rank(a_uzup))
+b = np.array([C, F])
+if np.linalg.matrix_rank(a)==np.linalg.matrix_rank(a_uzup)==2:
+    print("Równanie ma jedno rozwiązanie")
+    sol=(np.linalg.solve(a,b))
+    print('x = %5.2f , y = %5.2f' % (sol[0], sol[1]))
+elif np.linalg.matrix_rank(a)==np.linalg.matrix_rank(a_uzup)<2:
+    print("Układ ma nieskończenie wiele rozwiązań")
+else:
+    print("Układ równań jest układem sprzecznym")
