24b893986d60b61a4dbc87eccac888d2ae2e757a
Grzegorz Juszkiewicz <Grzegorz.Juszkiewicz@woodward.com>
2018-02-20 11:18:44 +0100

    Skończone zadanie 3

--- zadanie1.py
+++ zadanie1.py
@@ -1,21 +1,32 @@
-num = 1634
+#Funkcja tworzy palindrom z zadanej liczby
+def revNumber(num):
+    return int(str(num)[::-1])
 
-# Changed num variable to string, 
-# and calculated the length (number of digits)
-order = len(str(num))
+#Funkcja sprawdza czy liczba to palindom. Jeżeli jest zwraca True
+def isPalindrome(num):
+    return num==revNumber(num)
 
-# initialize sum
-sum = 0
-
-# find the sum of the cube of each digit
-temp = num
-while temp > 0:
-   digit = temp % 10
-   sum += digit ** order
-   temp //= 10
-
-# display the result
-if num == sum:
-   print(num,"is an Armstrong number")
-else:
-   print(num,"is not an Armstrong number")
+#Funkcja oblicza palindrom dla zadanej liczby startowej oraz maksymalnej liczbie iteracji  
+def findPalindrome(number,max_iter):
+    for num in range(0,number+1):
+        #Inicjalizacja licznika iteracji
+        count=0
+        #Inicjalizacja palindromu
+        rev_num=0
+        #Inicjalizacja sumy
+        sum=num
+        rev_num = revNumber(num)
+        #Szukaj sumy, sprawdź czy jest palidromem dopóki count<max_iter. Jeżeli isPalindrome(sum)-->true
+        # wypisz: liczbe startowa, liczbe iteracji, palindrom
+        #jezeli przekroczysz liczbe iteracji
+        #wypisz: liczbe startowa, ostatnią policzoną sumę
+        while (count<max_iter) :
+            count+=1
+            rev_num = revNumber(sum)
+            sum=sum+rev_num
+            if(isPalindrome(sum)):
+                print("Starting point: %i, Iteration number: %i, Palindrome: %i" % (num,count, sum))
+                break
+            elif(count>=max_iter):
+                print("FAILED. Starting point %i, Last iterated sum: %i"% (num,sum))
+    return
